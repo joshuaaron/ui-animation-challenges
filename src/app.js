0e3c582e3b9a7df6d3e6d4b01382c18f7b371181
Josh Reynolds <josh@askottentertainment.com>
2020-11-11 12:59:24 -0800

    feat: added base content to landing section

--- src/app.js
+++ src/app.js
@@ -1,3 +1,19 @@
 'use strict';
 
-document.getElementById('message').innerText = 'Hello World!';
+const box = document.getElementById('box');
+const animateRight = box.animate(
+    [{ transform: 'translateX(0)' }, { transform: 'translateX(200px)' }],
+    {
+        fill: 'forwards',
+        easing: 'ease-in-out',
+        duration: 2000,
+    }
+);
+
+animateRight.finished.then(() => {
+    box.animate([{ transform: 'translateY(0)' }, { transform: 'translateY(200px)' }], {
+        composite: 'add',
+        fill: 'both',
+        duration: 1500,
+    });
+});

e4d216c411bec586c09968309a55c9f484b2495b
Josh Reynolds <josh@askottentertainment.com>
2020-11-17 16:12:07 -0800

    refactor: remove WAAPI testing code

--- src/app.js
+++ src/app.js
@@ -1,19 +1,1 @@
 'use strict';
-
-const box = document.getElementById('box');
-const animateRight = box.animate(
-    [{ transform: 'translateX(0)' }, { transform: 'translateX(200px)' }],
-    {
-        fill: 'forwards',
-        easing: 'ease-in-out',
-        duration: 2000,
-    }
-);
-
-animateRight.finished.then(() => {
-    box.animate([{ transform: 'translateY(0)' }, { transform: 'translateY(200px)' }], {
-        composite: 'add',
-        fill: 'both',
-        duration: 1500,
-    });
-});
